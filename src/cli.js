98d81d762e415ddac9fbf0069486b64ad6d8ae45
Marcos <alvesmarcos.es@gmail.com>
2026-08-15 21:56:08 -0300

    add file saving functionality

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,28 @@ import { contaPalavras } from './index.js'
 
 const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
+const endereco = caminhoArquivo[3]
 
 fs.readFile(link, 'utf-8', (err, texto) => {
     try{
         if(err) throw err
-        contaPalavras(texto)
+        const resultado = contaPalavras(texto)
+        criarESalvarArquivo(resultado, endereco)
     }
     catch(err) {
         trataErros(err)
     }
 })
+
+async function criarESalvarArquivo(listaPalavras, endereco) {
+    const arquivoNovo = `${endereco}/resultado.txt`
+    const texto = JSON.stringify(listaPalavras)
+
+    try{
+        await fs.promises.writeFile(arquivoNovo, texto)
+        console.log('arquivo criado')
+    } 
+    catch(erro) {
+        throw erro
+    }
+}
